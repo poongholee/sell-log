707a6c31421eecb754867e9f75676236cb6d5cc6
SungJin-Jin <deskhi@nethru.co.kr>
2018-11-27 21:18:22 +0900

    [SPACE-26] Add koa-router

--- sell-log-backend/src/index.js
+++ sell-log-backend/src/index.js
@@ -1,11 +1,28 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx) => {
-    ctx.body = 'hello world';
+router.get('/', (ctx) => {
+    ctx.body = '홈';
 });
 
+
+router.get('/about/:name?', (ctx) => {
+    const { name } = ctx.params;
+
+    ctx.body = name ? `${name}의 소개` : `소개`;
+});
+
+router.get('/post', (ctx) => {
+    const { id } = ctx.query;
+
+    ctx.body = id ? `포스트 #${id}` : `포스트 아이디가 없습니다.`;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
 app.listen(4000, () => {
-    console.log('listen to port 4000');
+    console.log('listening to port 4000');
 });
