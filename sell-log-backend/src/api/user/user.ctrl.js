7fa3f2a333e6c38da1ff1e1983e682f42a05fe58
SungJin-Jin <deskhi@nethru.co.kr>
2018-12-16 14:53:01 +0900

    [SPACE-#40] Fix Validation Check

--- sell-log-backend/src/api/user/user.ctrl.js
+++ sell-log-backend/src/api/user/user.ctrl.js
@@ -1,12 +1,41 @@
 const User = require('models/User');
 
+const REGEX_USERNAME = /^[a-z0-9]+$/;
+
+const signUpValidateCheck = ((name, password) => {
+    if (password.length < 4 || typeof password !== "string") {
+        return {
+            status: 400,
+            body: {
+                error: "BAD PASSWORD"
+            }
+        }
+    }
+
+    return {
+        status: 200
+    }
+});
+
 exports.signUp = async (ctx) => {
     const { email, password, name } = ctx.request.body;
+
+    const result = signUpValidateCheck(name, password);
+
+    if (result.status !== 200) {
+        ctx.status = result.status;
+        ctx.body = result.body;
+
+        return;
+    }
+
     const user = new User({
         email: email,
         password: password,
         name: name
     });
+
+    user.password = user.generateHash(user.password);
 
     try {
         await user.save();
@@ -22,13 +51,17 @@ exports.login = async (ctx) => {
 
     try {
         const user = await User.findOne({
-            email: email,
-            password: password
+            email: email
         }).exec();
 
-        if(!user) {
+        if (!user || user.validateHash(password)) {
             ctx.status = 404;
             return;
+        }
+
+        ctx.session.user = {
+            _id: user._id,
+            name: user.name
         }
 
         ctx.body = user;
--- sell-log-backend/src/models/User.js
+++ sell-log-backend/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
 
 const { Schema } = mongoose;
 
@@ -8,6 +9,14 @@ const UserSchema = new Schema({
     password: { type: String, required: true },
     createdAt: { type: Date, default: new Date() }
 });
+
+UserSchema.methods.generateHash = function(password) {
+    return bcryptjs.hashSync(password, 0);
+}
+
+UserSchema.methods.validateHash = function(password) {
+    return bcryptjs.compareSync(password, this.password);
+}
 
 global.User = global.User || mongoose.model('User', UserSchema);
 module.exports = global.User;
